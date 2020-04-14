a93cc3ec6840563da94d94f6146c2b8b57e37c8f
Gurpreet Singh <gurpreetpannu20@yahoo.com>
2020-04-14 20:01:24 +1200

    Add project files.

---
+++ KangarooRace/PunterFactory.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace KangarooRace
+{
+    public class PunterFactory
+    {
+        public Punter getPunter(String Name, Label MaximumBet, Label bet)
+        {
+            Punter p;
+            switch (Name.ToLower())
+            {
+                case "gurpreet":
+                    p = new Gurpreet(null, MaximumBet, 50, bet);
+                    break;
+
+                case "raj":
+                    p = new Raj(null, MaximumBet, 50, bet);
+                    break;
+
+                case "harman":
+                    p = new Harman(null, MaximumBet, 50, bet);
+                    break;
+
+                default:
+                    p = null;
+                    break;
+            }
+            p.setPunterName();
+            return p;
+        }
+    }
+
+    public class Gurpreet : Punter
+    {
+        public Gurpreet(Bet MyBet, Label MaximumBet, int Cash, Label MyLabel) : base(MyBet, MaximumBet, Cash, MyLabel)
+        {
+        }
+
+        public override void setPunterName()
+        {
+            Name = "Gurpreet";
+        }
+    }
+    public class Harman : Punter
+    {
+        public Harman(Bet bet, Label MaximumBet, int Cash, Label label) : base(bet, MaximumBet, Cash, label)
+        {
+        }
+
+        public override void setPunterName()
+        {
+            Name = "Harman";
+        }
+    }
+    public class Raj : Punter
+    {
+        public Raj(Bet bet, Label MaximumBet, int Cash, Label label) : base(bet, MaximumBet, Cash, label)
+        {
+        }
+
+        public override void setPunterName()
+        {
+            Name = "Raj";
+        }
+    }
+}
---
+++ KangarooRaceTest/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using KangarooRace;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KangarooRaceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        PunterFactory pFactory = new PunterFactory();
+        Punter Gurpreet;
+        Kangaroo[] Kangaroos = new Kangaroo[2];
+
+        [TestMethod]
+        public void TestWinnerOutcome()
+        {
+            Kangaroo.StartingPosition1 = 0;
+            Kangaroo.RacetrackLength1 = 50;
+            int BettingAmount = 45;
+            int KangarooNumber = 1;
+            int expectedWin = 90;
+            int expectedLose = 0;
+            Kangaroos[0] = new Kangaroo() { KangarooPictureBox = null };
+            Kangaroos[1] = new Kangaroo() { KangarooPictureBox = null };
+            Gurpreet = pFactory.getPunter("Gurpreet", null, null);
+            Gurpreet.Cash = BettingAmount;
+            Gurpreet.PlaceBet((int)BettingAmount, KangarooNumber);
+
+            bool nowin = true;
+            int win = -1;
+            while (nowin)
+            {
+                for (int i = 0; i < Kangaroos.Length; i++)
+                {
+                    if (Kangaroo.Run(Kangaroos[i]))
+                    {
+                        win = i + 1;
+                        Gurpreet.Collect(win);
+                        nowin = false;
+
+                    }
+                }
+            }
+            if (Gurpreet.bet.KangarooNum == win)
+            {
+                Assert.AreEqual(expectedWin, Gurpreet.Cash, "Account not credited correctly");
+            }
+            if (Gurpreet.bet.KangarooNum != win)
+            {
+                Assert.AreEqual(expectedLose, Gurpreet.Cash, "Account not debited correctly");
+
+            }
+        }
+    }
+
+}
